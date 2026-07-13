4b28f0406af8bd4022a297f4db5a752cf3c362ca
mrzunairkhalid-jpg <mrzunairkhalid@gmail.com>
2026-07-13 14:42:00 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,38 @@
+const CACHE_NAME = "gold-converter-v1";
 
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./BB1D3160-126D-4CB5-8720-3C727001BBF1.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
